e8e325a8cae6605976f631b6501b49cd6d1b4342
Utkarsa Anand <utkarsa007>
2021-06-24 17:35:39 +0530

    spring project

--- src/main/java/com/springrest/springrest/controller/MyController.java
+++ src/main/java/com/springrest/springrest/controller/MyController.java
@@ -3,6 +3,9 @@ package com.springrest.springrest.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,10 +51,22 @@ public class MyController {
 		return this.courseService.addCourse(course);
 	}
 	
-	@PutMapping("/course/{courseId}")
+	@PutMapping("/Courses/{courseId}")
 	public Course updateCourse(@RequestBody Course course)
 	{
 		return this.courseService.updateCourse(course);
 	}
 	
+	@DeleteMapping("/courses/{courseId}")
+	
+public ResponseEntity<HttpStatus> deleteCourse(@PathVariable String courseId) {
+		try {
+			this.courseService.deleteCourse(Long.parseLong(courseId));
+			return new ResponseEntity<>(HttpStatus.OK);
+		}
+		catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 }
+
--- src/main/java/com/springrest/springrest/services/CourseService.java
+++ src/main/java/com/springrest/springrest/services/CourseService.java
@@ -13,5 +13,7 @@ public interface CourseService {
 	public Course addCourse(Course course);
 
 	public Course updateCourse(Course course);
+
+	public void deleteCourse(long parseLong);
 }
  
